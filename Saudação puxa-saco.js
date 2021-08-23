8f07f423f067a99fd5590166c661f5a75aee66d9
caamdoel <89041061+caamdoel@users.noreply.github.com>
2021-08-22 22:37:09 -0300

    Add files via upload

---
+++ Saudação puxa-saco.js
@@ -0,0 +1,12 @@
+/*Saudação puxa-saco*/
+/*requisito:
+/O programa 'Saudação puxa-saco' faz todos os cumprimentos do dia de uma vez só,
+ para a semana toda! Serão mostradas as três mensagens de tela,
+ uma após a outra, por dia, de segunda a sexta, cada dia em uma linha.
+*/
+console.log("Passei para desejar");
+
+console.log("Bom dia!");
+console.log("Boa tarde!");
+console.log("Boa noite!");
+
